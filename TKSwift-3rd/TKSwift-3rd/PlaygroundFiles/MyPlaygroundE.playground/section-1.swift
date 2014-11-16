d92d6caa8cbd378011154db7648e0f7c8da10531
Nicolastinkl <nicolastinkl@gmail.com>
2014-11-16 21:40:15 +0800

    add pattend mode

--- TKSwift-3rd/TKSwift-3rd/PlaygroundFiles/MyPlaygroundE.playground/section-1.swift
+++ TKSwift-3rd/TKSwift-3rd/PlaygroundFiles/MyPlaygroundE.playground/section-1.swift
@@ -4,4 +4,74 @@ import Cocoa
 
 var str = "Hello, playground"
 // print
-println(str)
+
+
+/*!
+*  @author tinkl, 14-11-16 21:11:45
+*
+*  正则表达式
+*
+*  @since 1.0.0
+*/
+struct ERgexHelper {
+    let regex:NSRegularExpression
+    
+    init(_ pattern:String)
+    {
+        var error : NSError?
+        
+        regex = NSRegularExpression(pattern: pattern, options: NSRegularExpressionOptions.CaseInsensitive, error: &error)!
+            
+    }
+    
+    
+    /*!
+    处理
+    
+    :param: input 输入源
+    
+    :returns: 是否匹配
+    */
+    func match(input:String) -> Bool
+    {
+        let matchs = regex.matchesInString(input, options: nil, range: NSMakeRange(0, countElements(input)))
+        return matchs.count > 0;
+    }
+    
+    
+    
+}
+
+
+let mailPattern = "^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$"
+
+let matcher = ERgexHelper(mailPattern)
+
+let maybeMail = "nicolastinkl@gmail.com"
+
+if matcher.match(maybeMail)
+{
+    println("OK")
+}else{
+    println("invaild email address")
+}
+
+
+
+
+infix operator =~~ {    associativity none    precedence 160}
+
+func =~~(lhs:String,rhs:String)->Bool
+{
+    return ERgexHelper(rhs).match(lhs)
+}
+
+
+if "asdfhjasklf@gmail.com" =~~ mailPattern
+{
+    println("OK")
+}
+
+
+
+
