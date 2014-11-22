982a4110dd4436b474ec93129e8c3f4f8d03b96e
Nicolastinkl <nicolastinkl@gmail.com>
2014-11-22 16:57:27 +0800

    update

--- TKSwift-3rd/TKSwift-3rd/PlaygroundFiles/MyPlaygroundF.playground/section-1.swift
+++ TKSwift-3rd/TKSwift-3rd/PlaygroundFiles/MyPlaygroundF.playground/section-1.swift
@@ -3,3 +3,30 @@
 import Cocoa
 
 var str:String = "ok"
+
+
+var nameArray = ["1 ","2","3"]
+
+
+
+
+
+func 犯法()
+{
+    println("OK")
+}
+
+犯法()
+
+
+println("\(犯法())")
+
+
+var i  =  0...4
+//0 1 2 3 4
+
+enumerate(i)
+
+
+
+
